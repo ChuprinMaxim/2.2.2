675485ac0ba02e72f55f0374c2f262b7f91b3b4f
ChuprinMaxim <bugsfix@mail.ru>
2022-04-23 20:58:31 +0500

    Initial commit

---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,28 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarService {
+    public List<Car> getCars() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Mitsu", "MonteroSport", "green"));
+        cars.add(new Car("BMW", "320", "red"));
+        cars.add(new Car("LADA", "Priora", "white"));
+        cars.add(new Car("BMW", "318", "black"));
+        cars.add(new Car("Mazda", "Cronos", "blue"));
+        return cars;
+    }
+
+    public List<Car> showAllCars(Integer count) {
+        if (count == null) {
+            count = getCars().size();
+        }
+        int getCount = count < getCars().size() ? count : getCars().size();
+        return getCars().subList(0, getCount);
+    }
+}
